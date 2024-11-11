1d297630d2dde139eae9940562967e0f781e1d9f
8glaney5 <laneylougoldman@gmail.com>
2024-11-11 12:22:02 -0500

    Fixed Client GUI constructor.

--- src/clack/endpoint/ClientGUI.java
+++ src/clack/endpoint/ClientGUI.java
@@ -15,6 +15,8 @@ import java.awt.*;
  * //TODO: How does it end?
  */
 
+//TODO: FIX SYNTAX ERRORS
+
 public class ClientGUI
 {
     /**
@@ -24,50 +26,48 @@ public class ClientGUI
     {
         final Border BORDER =
                 BorderFactory.createLineBorder(Color.LIGHT_GRAY);
-    }
 
-    // Connection panel: host, port, buttons
-    final JLabel hostLabel = new JLabel("Host: ");
-    final JTextField hostField = new JTextField("localhost", 20);
-    final JLabel portLabel = new JLabel("Port: ");
-    final JTextField portField = new JTextField("4466", 5);
-    final JPanel connectionInfoPanel = new JPanel();
-        connectionInfoPanel.setLayout(
-                new BoxLayout(connectionInfoPanel, BoxLayout.LINE_AXIS));
+        // Connection panel: host, port, buttons
+        final JLabel hostLabel = new JLabel("Host: ");
+        final JTextField hostField = new JTextField("localhost", 20);
+        final JLabel portLabel = new JLabel("Port: ");
+        final JTextField portField = new JTextField("4466", 5);
+        final JPanel connectionInfoPanel = new JPanel();
+        connectionInfoPanel.setLayout(new BoxLayout(connectionInfoPanel, BoxLayout.LINE_AXIS));
         connectionInfoPanel.add(hostLabel);
         connectionInfoPanel.add(hostField);
         connectionInfoPanel.add(portLabel);
         connectionInfoPanel.add(portField);
 
-    // Connect and Disconnect buttons, each centered in their own
-    // small pane, and these panes side-by-side.
-    final JButton connectButton = new JButton("Connect");
-    final JButton disconnectButton = new JButton("Disconnect");
+        // Connect and Disconnect buttons, each centered in their own
+        // small pane, and these panes side-by-side.
+        final JButton connectButton = new JButton("Connect");
+        final JButton disconnectButton = new JButton("Disconnect");
 
-    final JPanel connectionButtonsPanel = new JPanel();
+        final JPanel connectionButtonsPanel = new JPanel();
         connectionButtonsPanel.setLayout(new FlowLayout());
         connectionButtonsPanel.add(connectButton);
         connectionButtonsPanel.add(Box.createRigidArea((new Dimension(20, 0))));
         connectionButtonsPanel.add(disconnectButton);
 
-    final JPanel connectionPanel = new JPanel();
+        final JPanel connectionPanel = new JPanel();
         connectionPanel.setBorder(BORDER);
         connectionPanel.setLayout(
                 new BoxLayout(connectionPanel, BoxLayout.PAGE_AXIS));
         connectionPanel.add(connectionInfoPanel);
         connectionPanel.add(connectionButtonsPanel);
 
-    // Build logPane: conversation log
-    final JTextArea log = new JTextArea();
+        // Build logPane: conversation log
+        final JTextArea log = new JTextArea();
         log.setEditable(false);
         log.setRows(6);
         log.setLineWrap(true);
-    final JScrollPane logPane = new JScrollPane(log);
+        final JScrollPane logPane = new JScrollPane(log);
 
-    // Build questionPanel: where user enters questions
-    final JLabel questionLabel = new JLabel("Your message: ");
-    final JTextField questionField = new JTextField(30);
-    final JPanel questionPanel = new JPanel();
+        // Build questionPanel: where user enters questions
+        final JLabel questionLabel = new JLabel("Your message: ");
+        final JTextField questionField = new JTextField(30);
+        final JPanel questionPanel = new JPanel();
         questionPanel.setBorder(BORDER);
         questionPanel.setLayout(
                 new BoxLayout(questionPanel, BoxLayout.LINE_AXIS));
@@ -76,8 +76,8 @@ public class ClientGUI
         questionPanel.add(questionLabel);
         questionPanel.add(questionField);
 
-    // Put panels together
-    final JFrame frame = new JFrame();
+        // Put panels together
+        final JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Clack");
         frame.setLayout(new BorderLayout());
@@ -85,32 +85,33 @@ public class ClientGUI
         frame.add(logPane, BorderLayout.CENTER);
         frame.add(questionPanel, BorderLayout.SOUTH);
 
-    // Wire up the listeners
+        // Wire up the listeners
         connectButton.addActionListener((e) ->
-    {
-        log.append("Connect Button clicked, connect to "
-                + hostField.getText() + ":" + portField.getText() + "\n");
-        hostField.setEnabled(false);
-        portField.setEnabled(false);
-        connectButton.setEnabled(false);
-    }
+                {
+                    log.append("Connect Button clicked, connect to "
+                            + hostField.getText() + ":" + portField.getText() + "\n");
+                    hostField.setEnabled(false);
+                    portField.setEnabled(false);
+                    connectButton.setEnabled(false);
+                }
         );
         disconnectButton.addActionListener((e) ->
-    {
-        log.append("Disconnect Button clicked\n");
-        hostField.setEnabled(true);
-        portField.setEnabled(true);
-        connectButton.setEnabled(true);
-    }
+                {
+                    log.append("Disconnect Button clicked\n");
+                    hostField.setEnabled(true);
+                    portField.setEnabled(true);
+                    connectButton.setEnabled(true);
+                }
         );
         questionField.addActionListener((e) ->
-        log.append("User entered: '" + questionField.getText() + "'\n")
+                log.append("User entered: '" + questionField.getText() + "'\n")
         );
 
         frame.pack();
         frame.setLocation(x, y);
         frame.setVisible(true);
-}
+    }
+
 
 
 }
